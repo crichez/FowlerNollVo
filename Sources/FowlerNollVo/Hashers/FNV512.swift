59c1341dbad2346496fa93affc0264a5e980153d
Christopher Richez <crichez@pm.me>
2022-02-03 20:10:49 -0500

    added the `Digest512` type alias for code clarity

--- Sources/FowlerNollVo/Hashers/FNV512.swift
+++ Sources/FowlerNollVo/Hashers/FNV512.swift
@@ -5,38 +5,45 @@
 //  Created by Christopher Richez on 2/2/22.
 //
 
-extension DoubleWidth where Base == DoubleWidth<DoubleWidth<UInt64>> {
-    fileprivate static var fnvPrime: DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>> {
-        DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>>(
-            DoubleWidth<DoubleWidth<UInt64>>(
-                DoubleWidth<UInt64>(0x0000000000000000, 0x0000000000000000),
-                DoubleWidth<UInt64>(0x0000000001000000, 0x0000000000000000)
+/// A 128-bit digest that behaves like an unsigned integer.
+///
+/// `DoubleWidth` is from the [Swift numerics](https://github.com/apple/swift-numerics)
+/// open-source project. Expect all operations on a `Digest512` value to perform
+/// noticeably worse than operations on `UInt64` and other standard library native integers.
+public typealias Digest512 = DoubleWidth<Digest256>
+
+extension Digest512 {
+    fileprivate static var fnvPrime: Digest512 {
+        Digest512(
+            Digest256(
+                Digest128(0x0000000000000000, 0x0000000000000000),
+                Digest128(0x0000000001000000, 0x0000000000000000)
             ),
-            DoubleWidth<DoubleWidth<UInt64>>(
-                DoubleWidth<UInt64>(0x0000000000000000, 0x0000000000000000),
-                DoubleWidth<UInt64>(0x0000000000000000, 0x0000000000000157)
+            Digest256(
+                Digest128(0x0000000000000000, 0x0000000000000000),
+                Digest128(0x0000000000000000, 0x0000000000000157)
             )
         )
     }
     
-    fileprivate static var fnvOffset: DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>> {
-        DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>>(
-            DoubleWidth<DoubleWidth<UInt64>>(
-                DoubleWidth<UInt64>(0xb86db0b1171f4416, 0xdca1e50f309990ac),
-                DoubleWidth<UInt64>(0xac87d059c9000000, 0x0000000000000d21)
+    fileprivate static var fnvOffset: Digest512 {
+        Digest512(
+            Digest256(
+                Digest128(0xb86db0b1171f4416, 0xdca1e50f309990ac),
+                Digest128(0xac87d059c9000000, 0x0000000000000d21)
             ),
-            DoubleWidth<DoubleWidth<UInt64>>(
-                DoubleWidth<UInt64>(0xe948f68a34c192f6, 0x2ea79bc942dbe7ce),
-                DoubleWidth<UInt64>(0x182036415f56e34b, 0xac982aac4afe9fd9)
+            Digest256(
+                Digest128(0xe948f68a34c192f6, 0x2ea79bc942dbe7ce),
+                Digest128(0x182036415f56e34b, 0xac982aac4afe9fd9)
             )
         )
     }
 }
 
 
-/// A `FNV-1` hasher with a `DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>>` digest.
+/// A `FNV-1` hasher with a `Digest512` digest.
 public struct FNV512: FNVHasher {
-    public private(set) var digest: DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>>
+    public private(set) var digest: Digest512
     
     public init() {
         self.digest = .fnvOffset
@@ -64,9 +71,9 @@ public struct FNV512: FNVHasher {
     }
 }
 
-/// A `FNV-1a` hasher with a `DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>>` digest.
+/// A `FNV-1a` hasher with a `Digest512` digest.
 public struct FNV512a: FNVHasher {
-    public private(set) var digest: DoubleWidth<DoubleWidth<DoubleWidth<UInt64>>>
+    public private(set) var digest: Digest512
     
     public init() {
         self.digest = .fnvOffset
