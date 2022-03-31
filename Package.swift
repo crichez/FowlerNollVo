6943e677f350a6e934616dd7d61188be86075464
Christopher Richez <crichez@pm.me>
2022-03-30 22:08:35 -0500

    Added documentation static page source.

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
             name: "FowlerNollVo",
             targets: ["FowlerNollVo"]),
     ],
+    dependencies: [
+        //.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "FowlerNollVo",
